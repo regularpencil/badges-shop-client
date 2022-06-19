e0b4cbf558e38a6784a2eaa4d10931b3431414a3
regularpencil <regularpencil@yandex.ru>
2022-06-19 21:18:58 +0700

    deploy-2

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -2,18 +2,18 @@ import { $api } from "../http";
 
 export default class UserService {
     static async saveFavorites(email, favorites) {
-        return $api.post("http://localhost:4000/api/favorites", {email, favorites});
+        return $api.post("https://badges-shop-server.herokuapp.com/api/favorites", {email, favorites});
     }
 
     static async saveHistory(email, history) {
-        return $api.post("http://localhost:4000/api/history", {email, history});
+        return $api.post("https://badges-shop-server.herokuapp.com/api/history", {email, history});
     }
 
     static async saveOrders(email, orders) {
-        return $api.post("http://localhost:4000/api/orders", {email, orders});
+        return $api.post("https://badges-shop-server.herokuapp.com/api/orders", {email, orders});
     }
 
     static async saveUserSettings(email, phoneNumber, name) {
-        return $api.post("http://localhost:4000/api/settings", {email, phoneNumber, name});
+        return $api.post("https://badges-shop-server.herokuapp.com/api/settings", {email, phoneNumber, name});
     }
 }
